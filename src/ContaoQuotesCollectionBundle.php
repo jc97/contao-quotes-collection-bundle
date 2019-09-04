375f5f0bfafe0ea0bbaf36c2e38f0a211f913c1f
Julian Knorr <git@jknorr.eu>
2019-09-04 23:06:20 +0200

    Initial commit

---
+++ src/ContaoQuotesCollectionBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace jc97\ContaoQuotesCollectionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoQuotesCollectionBundle extends Bundle
+{
+}
