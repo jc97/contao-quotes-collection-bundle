fb213f72620cea555f086c686e0e3cb73b5dbdd6
Julian Knorr <git@jknorr.eu>
2019-09-05 03:46:05 +0200

    Add module configuration palette.

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,4 @@
+<?php
+
+// Add palettes to tl_module
+$GLOBALS['TL_DCA']['tl_module']['palettes']['quotes']           = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
