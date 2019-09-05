a129805bc6368a14d709a9cf650744a943ff5308
Julian Knorr <git@jknorr.eu>
2019-09-05 16:37:21 +0200

    Add English tranlation

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS['TL_LANG']['MOD']['quotes']['0']                    = 'Quotes';
+$GLOBALS['TL_LANG']['MOD']['quotes']['1']                    = 'Manage Quotes Collection';
+$GLOBALS['TL_LANG']['FMD']['quotes']['0']                    = 'Quote';
+$GLOBALS['TL_LANG']['FMD']['quotes']['1']                    = 'Prints a random quote of internal collection.';
---
+++ src/Resources/contao/languages/en/tl_quotes.php
@@ -0,0 +1,17 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_quotes']['tstamp']['0']              = 'Revision date';
+$GLOBALS['TL_LANG']['tl_quotes']['tstamp']['1']              = 'Date and time of the latest revision';
+$GLOBALS['TL_LANG']['tl_quotes']['author']['0']              = 'Originator';
+$GLOBALS['TL_LANG']['tl_quotes']['author']['1']              = 'Quote\'s Originator (leave blank if unknown)';
+$GLOBALS['TL_LANG']['tl_quotes']['text']['0']                = 'Quote';
+$GLOBALS['TL_LANG']['tl_quotes']['text']['1']                = 'Quote\'s Text';
+$GLOBALS['TL_LANG']['tl_quotes']['published']['0']           = 'Publish';
+$GLOBALS['TL_LANG']['tl_quotes']['published']['1']           = 'Make the quote publicly visible on the website.';
+$GLOBALS['TL_LANG']['tl_quotes']['delete']['0']              = 'Delete Quote';
+$GLOBALS['TL_LANG']['tl_quotes']['delete']['1']              = 'Delete Quote ID %s';
+$GLOBALS['TL_LANG']['tl_quotes']['new']['0']                 = 'New Quote';
+$GLOBALS['TL_LANG']['tl_quotes']['new']['1']                 = 'Add a new Quote';
+$GLOBALS['TL_LANG']['tl_quotes']['edit']['0']                = 'Edit Quote';
+$GLOBALS['TL_LANG']['tl_quotes']['edit']['1']                = 'Edit Quote ID %s';
+$GLOBALS['TL_LANG']['tl_quotes']['quote_legend']             = 'Quote';
